b03574aa06cb57e0523d188df48a3d78824bb1bb
yobulkmaster <master@yobulk.dev>
2022-11-28 22:46:32 +0530

    removed unused imports

--- components/errorpanel/index.jsx
+++ components/errorpanel/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Collapsible from 'react-collapsible';
 
 const Modal = ({ isOpen, setIsOpen, data }) => {
   const [showModal, setShowModal] = useState(false);
