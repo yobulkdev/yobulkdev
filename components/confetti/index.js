e728905c835a7786c4af595e826ced95b5be914d
yobulkmaster <master@yobulk.dev>
2022-12-03 14:51:25 +0530

    fixed productio builld

--- components/confetti/index.js
+++ components/confetti/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import useWindowSize from 'react-use/lib/useWindowSize';
 import Confetti from 'react-confetti';
 
-export default () => {
+const ConfettiComponent = () => {
   const { width, height } = useWindowSize();
   return <Confetti width={width} height={height} className="duration-200" />;
 };
+
+export default ConfettiComponent;
