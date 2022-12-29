33d01deb2563f810f98b5fd69b3cecc1950027e2
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-12-29 10:32:03 +0530

    test

--- components/Collaborate/CollaborateComponent.js
+++ components/Collaborate/CollaborateComponent.js
@@ -41,6 +41,7 @@ const CollaborateComponent = () => {
                                     dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder='Enter Workspace Name Here ...'
                     />
+                    this is random
                 </div>
             </div>
         </>
