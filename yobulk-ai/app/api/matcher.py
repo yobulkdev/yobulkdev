0ff2c4a4c66b87c56e2ddd2cce5317cb792e6b0b
samurai2y <samurai2@yobulk.dev>
2023-02-01 22:04:17 +0530

    added bert model

--- yobulk-ai/app/api/matcher.py
+++ yobulk-ai/app/api/matcher.py
@@ -1,12 +1,14 @@
 from typing import Any, List
 from fastapi import APIRouter
-# from app.lib.string_matcher import similarity_score
+from app.lib.string_matcher import bert_tfidf_match
 
 router = APIRouter()
 
 
-@router.post("/string", status_code=201)
-def match_string(text1: List[str], text2: List[str]) -> Any:
-    # score = similarity_score(text1, text2)
-    return {"score": 100}
+@router.post("/string", status_code=200)
+def match_list(lst1: List[str], lst2: List[str]) -> Any:
+    # lst1 = ['EMP_NAME', 'EMP_AGE', 'EMP_DOJ', 'EMP_MAIL_ID']
+    # lst2 = ['name', 'email', 'doj', 'age']
+    results = bert_tfidf_match(lst1, lst2)
+    return results
     
--- yobulk-ai/app/lib/string_matcher.py
+++ yobulk-ai/app/lib/string_matcher.py
@@ -1,53 +1,22 @@
-# import tensorflow as tf
-# import tensorflow_hub as hub
-# import numpy as np
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from transformers import BertTokenizer
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+import torch
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+model = AutoModelForMaskedLM.from_pretrained("bert-base-uncased")
 
-# # Load the BERT model from TensorFlow Hub
-# bert_model = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1"
-# bert_layer = hub.Module(bert_model)
-
-# # Load the BERT tokenizer from the Hugging Face Transformers library
-# tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-# # Create a function to calculate the BERT representation of each string
-# def bert_representation(strings):
-#     # Convert the strings to a list of input examples
-#     input_examples = [{"input_ids": [101] + [tokenizer.vocab[token] for token in string.split()] + [102]}
-#                       for string in strings]
-#     # Pass the inputs through the BERT layer to get the outputs
-#     bert_outputs = bert_layer(input_examples)
-#     # Extract the last layer of the outputs
-#     last_layer = bert_outputs["sequence_output"]
-#     return last_layer
-
-# # Use the TfidfVectorizer to calculate the TF-IDF representation of each string
-# vectorizer = TfidfVectorizer()
-
-# def tfidf_representation(strings):
-#     # Fit the vectorizer on the strings
-#     vectorizer.fit_transform(strings)
-#     # Transform the strings into their TF-IDF representations
-#     tfidf_representations = vectorizer.transform(strings)
-#     return tfidf_representations
-
-# # Concatenate the BERT and TF-IDF representations to form a combined representation
-# def combined_representation(strings):
-#     bert_reps = bert_representation(strings)
-#     tfidf_reps = tfidf_representation(strings)
-#     combined_reps = np.concatenate([bert_reps, tfidf_reps], axis=-1)
-#     return combined_reps
-
-# # Use the combined representation to create a similarity score between the two strings
-# def similarity_score(string1, string2):
-#     strings = [string1, string2]
-#     combined_reps = combined_representation(strings)
-#     score = np.dot(combined_reps[0], combined_reps[1])
-#     return score
-
-# # Example usage:
-# string1 = "string 1"
-# string2 = "string 2"
-# score = similarity_score(string1, string2)
-# print("Similarity score: ", score)
+def bert_tfidf_match(list1, list2):
+    # Tokenize the strings
+    encoded_list_1 = [tokenizer.encode_plus(string, return_tensors="pt", padding=True, truncation=True) for string in list1]
+    encoded_list_2 = [tokenizer.encode_plus(string, return_tensors="pt", padding=True, truncation=True) for string in list2]   
+    results = {}
+    for i, word1 in enumerate(encoded_list_1):
+        results[list1[i]] = ["", 0]
+        output1 = model(word1["input_ids"])[0].mean(dim=1)
+        for j, word2 in enumerate(encoded_list_2):
+            output2 = model(word2["input_ids"])[0].mean(dim=1)
+            cosine_similarity = torch.nn.functional.cosine_similarity(output1, output2).item()
+            if cosine_similarity > results[list1[i]][1]:
+                results[list1[i]][0] = list2[j]
+                results[list1[i]][1] = cosine_similarity
+    for i in list(results.keys()):
+        results[i] = results[i][0]
+    return results
