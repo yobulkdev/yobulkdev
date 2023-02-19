4ff00ae83b03f44e635ab90b82ef3a5a4f4f2d2f
yobulkmaster <master@yobulk.dev>
2023-02-19 09:11:43 +0530

    fix case issue

--- components/Configuration/index.js
+++
@@ -1,6 +0,0 @@
-export { default as AttachToImporter } from './AttachToImporter';
-export { default as AttachToOrganizations } from './AttachToOrganizations';
-export { default as AttachToJSONOBJ } from './AttachToJSONOBJ';
-export { default as AttachToWorkspace } from './AttachToWorkspace';
-export { default as AttachWebHookURL } from './AttachWebHookURL';
-export { default as ConfigurationConfig } from './ConfigurationConfig';
