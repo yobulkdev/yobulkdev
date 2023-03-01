bd098c96745d41e87a00180777933ad8776fec9d
yobulkmaster <master@yobulk.dev>
2023-03-01 22:55:16 +0530

    chaged some color scheme

--- components/demo/DemoCard.jsx
+++ components/demo/DemoCard.jsx
@@ -69,7 +69,7 @@ const DemoCard = ({ item }) => {
         </div>
         <div className="flex items-center gap-2">
           <Link href={item.downloadLink}>
-            <button className="bg-[#5EB4EA] w-[160px] h-[36px]  text-sm text-white p-2 rounded-md dark:bg-white dark:text-black">
+            <button className="bg-[#C83] w-[160px] h-[36px]  text-sm text-white p-2 rounded-md dark:bg-white dark:text-black">
               Download CSV
             </button>
           </Link>
@@ -82,7 +82,7 @@ const DemoCard = ({ item }) => {
 
           <div>
             <button
-              className="bg-blue-700 w-[160px] h-[36px]  text-sm text-white p-2 ml-12 rounded-md hover:bg-white hover:text-blue-700 hover:border hover:border-blue-700"
+              className="bg-gray-400 w-[160px] h-[36px]  text-sm text-white p-2 ml-12 rounded-md hover:bg-white hover:text-blue-700 hover:border hover:border-blue-700"
               onClick={() => setIsSchemaMenuOpen(true)}
             >
               View Schema
