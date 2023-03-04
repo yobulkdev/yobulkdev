c3c5486fc098347bba545cab22d04244ebdefa15
samurai2y <samurai2@yobulk.dev>
2023-03-04 16:23:12 +0530

    fixed code formatting

--- components/authguard/index.js
+++ components/authguard/index.js
@@ -1,30 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSession } from 'next-auth/react';
 import Login from '../../components/login';
-import { useRouter } from 'next/router';
 
 const AuthGuard = ({ children }) => {
-  // const [loading, setLoading] = useState(false);
-  // const [isValidSession, setValidSession] = useState(false);
-  // const router = useRouter();
   const { data: session } = useSession();
-
-  // useEffect(() => {
-  //   if (!router) return;
-  //   // if (
-  //   //   [
-  //   //     '/saasloader/[templateId]',
-  //   //     '/saasloadmatcher',
-  //   //     '/dataviewer/[viewtype]',
-  //   //   ].includes(router.pathname)
-  //   // ) {
-  //   //   setValidSession(true);
-  //   // }
-  //   if(session) {
-  //     setValidSession(true);
-  //   }
-  //   setLoading(false);
-  // }, [router, session]);
 
   return session ? <>{children}</> : <Login />;
 };
