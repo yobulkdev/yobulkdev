abbee0775d20ab21d861cef20fe72147f400d3e6
samurai2y <samurai2@yobulk.dev>
2023-03-04 16:20:39 +0530

    fixed refresh redirect

--- components/authguard/index.js
+++ components/authguard/index.js
@@ -4,29 +4,29 @@ import Login from '../../components/login';
 import { useRouter } from 'next/router';
 
 const AuthGuard = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [isValidSession, setValidSession] = useState(false);
-  const router = useRouter();
+  // const [loading, setLoading] = useState(false);
+  // const [isValidSession, setValidSession] = useState(false);
+  // const router = useRouter();
   const { data: session } = useSession();
 
-  useEffect(() => {
-    if (!router) return;
-    if (
-      [
-        '/saasloader/[templateId]',
-        '/saasloadmatcher',
-        '/dataviewer/[viewtype]',
-      ].includes(router.pathname)
-    ) {
-      setValidSession(true);
-    }
-    if (session) {
-      setValidSession(true);
-    }
-    setLoading(false);
-  }, [router, session]);
+  // useEffect(() => {
+  //   if (!router) return;
+  //   // if (
+  //   //   [
+  //   //     '/saasloader/[templateId]',
+  //   //     '/saasloadmatcher',
+  //   //     '/dataviewer/[viewtype]',
+  //   //   ].includes(router.pathname)
+  //   // ) {
+  //   //   setValidSession(true);
+  //   // }
+  //   if(session) {
+  //     setValidSession(true);
+  //   }
+  //   setLoading(false);
+  // }, [router, session]);
 
-  return loading ? <></> : <>{isValidSession ? <>{children}</> : <Login />} </>;
+  return session ? <>{children}</> : <Login />;
 };
 
 export default AuthGuard;
