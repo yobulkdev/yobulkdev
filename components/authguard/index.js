df78d2b76b8d5728851b150270b1884e82f3bd99
samurai2y <samurai2@yobulk.dev>
2023-03-04 16:09:30 +0530

    fixed auth error for imported pages

--- components/authguard/index.js
+++ components/authguard/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import Login from '../../components/login';
+import { useRouter } from 'next/router';
 
-const AuthGuard = ({children}) => {
+const AuthGuard = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [isValidSession, setValidSession] = useState(false);
+  const router = useRouter();
   const { data: session } = useSession();
-  return session ? (<>{children}</>) : (<Login />);
+
+  useEffect(() => {
+    if (!router) return;
+    if (
+      [
+        '/saasloader/[templateId]',
+        '/saasloadmatcher',
+        '/dataviewer/[viewtype]',
+      ].includes(router.pathname)
+    ) {
+      setValidSession(true);
+    }
+    if (session) {
+      setValidSession(true);
+    }
+    setLoading(false);
+  }, [router, session]);
+
+  return loading ? <></> : <>{isValidSession ? <>{children}</> : <Login />} </>;
 };
 
 export default AuthGuard;
