c69cc99c651d214a38050c05ecaa40111982bfec
samurai2y <samurai2@yobulk.dev>
2023-03-10 18:19:41 +0530

    working ai

--- pages/api/yobulk-ai/feedback.js
+++ pages/api/yobulk-ai/feedback.js
@@ -1,6 +1,7 @@
 import getGPTResponse from '../../../lib/gpt-engine';
 import clientPromise from '../../../lib/mongodb';
 
+
 export default async function feedback(req, res) {
   const queryParams = req.query;
   const { columnName, columnValue, collection } = queryParams;
@@ -19,20 +20,20 @@ export default async function feedback(req, res) {
         delete x['validationData'];
         return x;
       });
-      let actualPrompt = `You are a potential error finder on the following data. I am passing an array of object as input. For each object in this array, validate if the value sematically and logically matches for it or not. For example if age is 500. It is not a logical value. For output return the original array but add a feedback object in each array where you can add keys of that object and their feedback. The response should be an array only so i can parse it. \n
+      let actualPrompt = `You are a potential error finder on the following data. I am passing an array of object as input. For each object in this array, validate if the value sematically and logically matches for it or not. For example if age is 500 or weight is 700. It is not a logical value. For output return the original array but add a feedback object in each array where you can add keys of that object and their feedback. The response should be an array only so i can parse it. \n
       Input Array: ${JSON.stringify(parsedRows, 2, 0)}`;
       let originalResp = await getGPTResponse(actualPrompt, 1000, 0, 0);
       let resp = originalResp;
-      try{
-        resp = originalResp.split('Output Array: ')
-        resp = JSON.parse(resp[1])
-      }catch(e){
-        console.log(e)
+      try {
+        resp = originalResp.split('Output Array: ');
+        resp = JSON.parse(resp[1]);
+      } catch (e) {
+        console.log(e);
         resp = originalResp;
       }
       let rv = {};
-      for (const item of resp){
-        rv[item._id] = item
+      for (const item of resp) {
+        rv[item._id] = item;
       }
       res.json({ status: 200, data: rv });
       break;
